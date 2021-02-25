ace5ee7f724dde9798092a0fb89143d7b69ace6f
Logan Kulinski <lbkulinski@icloud.com>
2021-02-25 12:14:02 -0500

    Added additional code to PostView

--- src/main/java/PostView.java
+++ src/main/java/PostView.java
@@ -2,6 +2,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Tab;
+import java.util.Objects;
 
 /**
  * A view for posts of the Social Butterfly application.
@@ -10,11 +14,6 @@ import javafx.scene.layout.VBox;
  * February 25, 2021
  */
 public final class PostView {
-    /**
-     * The main box of this post view.
-     */
-    private final VBox mainBox;
-
     /**
      * The refresh button of this post view.
      */
@@ -46,6 +45,11 @@ public final class PostView {
     private final VBox allBox;
 
     /**
+     * The main box of this post view.
+     */
+    private final VBox mainBox;
+
+    /**
      * The scene of this post view.
      */
     private final Scene scene;
@@ -54,11 +58,9 @@ public final class PostView {
      * Constructs a newly allocated {@code PostView} object.
      */
     private PostView() {
-        String buttonText = "Refresh";
+        String refreshText = "Refresh";
 
-        this.mainBox = new VBox();
-
-        this.refreshButton = new Button(buttonText);
+        this.refreshButton = new Button(refreshText);
 
         this.tabPane = new TabPane();
 
@@ -70,17 +72,10 @@ public final class PostView {
 
         this.allBox = new VBox();
 
+        this.mainBox = new VBox(this.refreshButton, this.tabPane);
+
         this.scene = new Scene(this.mainBox);
     } //PostView
-
-    /**
-     * Returns the main box of this post view.
-     *
-     * @return the main box of this post view
-     */
-    public VBox getMainBox() {
-        return this.mainBox;
-    } //getMainBox
 
     /**
      * Returns the refresh button of this post view.
@@ -137,6 +132,15 @@ public final class PostView {
     } //getAllBox
 
     /**
+     * Returns the main box of this post view.
+     *
+     * @return the main box of this post view
+     */
+    public VBox getMainBox() {
+        return this.mainBox;
+    } //getMainBox
+
+    /**
      * Returns the scene of this post view.
      *
      * @return the scene of this post view
@@ -144,4 +148,77 @@ public final class PostView {
     public Scene getScene() {
         return this.scene;
     } //getScene
+
+    /**
+     * Creates, and returns, a {@code PostView} object using the specified primary stage.
+     *
+     * @param primaryStage the primary stage to be used in the operation
+     * @return a {@code PostView} object using the specified primary stage
+     * @throws NullPointerException if the specified primary stage is {@code null}
+     */
+    public static PostView createPostView(Stage primaryStage) {
+        PostView postView;
+        ScrollPane redditScrollPane;
+        ScrollPane twitterScrollPane;
+        ScrollPane instagramScrollPane;
+        ScrollPane allScrollPane;
+        Tab redditTab;
+        String redditText = "Reddit";
+        Tab twitterTab;
+        String twitterText = "Twitter";
+        Tab instagramTab;
+        String instagramText = "Instagram";
+        Tab allTab;
+        String allText = "All";
+
+        Objects.requireNonNull(primaryStage, "the specified primary stage is null");
+
+        postView = new PostView();
+
+        postView.refreshButton.prefWidthProperty()
+                              .bind(primaryStage.widthProperty());
+
+        redditScrollPane = new ScrollPane(postView.redditBox);
+
+        twitterScrollPane = new ScrollPane(postView.twitterBox);
+
+        instagramScrollPane = new ScrollPane(postView.instagramBox);
+
+        allScrollPane = new ScrollPane(postView.allBox);
+
+        redditTab = new Tab(redditText);
+
+        redditTab.setClosable(false);
+
+        redditTab.setContent(redditScrollPane);
+
+        twitterTab = new Tab(twitterText);
+
+        twitterTab.setClosable(false);
+
+        twitterTab.setContent(twitterScrollPane);
+
+        instagramTab = new Tab(instagramText);
+
+        instagramTab.setClosable(false);
+
+        instagramTab.setContent(instagramScrollPane);
+
+        allTab = new Tab(allText);
+
+        allTab.setClosable(false);
+
+        allTab.setContent(allScrollPane);
+
+        postView.tabPane.getTabs()
+                        .addAll(redditTab, twitterTab, instagramTab, allTab);
+
+        postView.tabPane.prefWidthProperty()
+                        .bind(postView.scene.widthProperty());
+
+        postView.tabPane.prefHeightProperty()
+                        .bind(postView.scene.heightProperty());
+
+        return postView;
+    } //createPostView
 }
