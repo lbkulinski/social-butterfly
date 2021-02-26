4251d55a62da4ac2ffb24860baab68afb3d601bb
Logan Kulinski <lbkulinski@icloud.com>
2021-02-26 12:05:01 -0500

    Added support for inline images and videos

--- src/main/java/TwitterExample.java
+++ src/main/java/TwitterExample.java
@@ -1,34 +1,30 @@
-import javafx.application.Application;
-import java.io.File;
-import java.io.ObjectInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import com.butterfly.social.PostView;
 import com.butterfly.social.User;
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import twitter4j.TwitterException;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.layout.VBox;
-import twitter4j.Status;
-import javafx.scene.Scene;
-import java.time.LocalDateTime;
-import javafx.scene.control.Label;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import twitter4j.MediaEntity;
+import twitter4j.RateLimitStatus;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoField;
-import javafx.stage.Stage;
-import com.butterfly.social.PostView;
-import javafx.scene.control.Button;
-import java.util.Set;
-import java.util.HashSet;
-import twitter4j.RateLimitStatus;
-import java.util.List;
-import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import java.util.ArrayList;
-import javafx.scene.control.Separator;
-import java.io.ObjectOutputStream;
-import java.io.FileOutputStream;
+import java.util.*;
 
 public final class TwitterExample extends Application {
     private User getUser() throws TwitterException {
@@ -76,6 +72,128 @@ public final class TwitterExample extends Application {
         return user;
     } //getUser
 
+    private Accordion getMediaAccordion(MediaEntity[] mediaEntities) {
+        List<TitledPane> titledPanes;
+        String type;
+        String videoType = "video";
+        String urlString;
+        URI uri;
+        String uriString;
+        Node node;
+        String mediaName;
+        int mediaCount = 0;
+        ScrollPane scrollPane;
+        TitledPane titledPane;
+        TitledPane[] array;
+        Accordion accordion;
+
+        Objects.requireNonNull(mediaEntities, "the specified array of media entities is null");
+
+        if (mediaEntities.length == 0) {
+            return null;
+        } //end if
+
+        titledPanes = new ArrayList<>();
+
+        for (MediaEntity mediaEntity : mediaEntities) {
+            type = mediaEntity.getType();
+
+            if (Objects.equals(type, videoType)) {
+                MediaEntity.Variant[] variants;
+                int lastIndex;
+                Media media;
+                MediaPlayer mediaPlayer;
+                MediaView mediaView;
+
+                variants = mediaEntity.getVideoVariants();
+
+                Arrays.sort(variants, Comparator.comparing(MediaEntity.Variant::getBitrate));
+
+                lastIndex = variants.length - 1;
+
+                urlString = variants[lastIndex].getUrl();
+
+                System.out.printf("Video urlString: %s%n", urlString);
+
+                try {
+                    uri = new URI(urlString);
+
+                    uriString = uri.toString();
+
+                    System.out.printf("Video uriString: %s%n", uriString);
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+
+                    continue;
+                } //end try catch
+
+                media = new Media(uriString);
+
+                mediaPlayer = new MediaPlayer(media);
+
+                mediaView = new MediaView(mediaPlayer);
+
+                mediaPlayer.setOnEndOfMedia(mediaPlayer::stop);
+
+                mediaView.setOnMouseClicked((mouseEvent) -> {
+                    MediaPlayer.Status status;
+
+                    status = mediaPlayer.getStatus();
+
+                    switch (status) {
+                        case PLAYING -> mediaPlayer.pause();
+                        case READY, PAUSED, STOPPED -> mediaPlayer.play();
+                    } //end switch
+                });
+
+                node = mediaView;
+            } else {
+                Image image;
+                ImageView imageView;
+
+                urlString = mediaEntity.getMediaURLHttps();
+
+                System.out.printf("Image urlString: %s%n", urlString);
+
+                try {
+                    uri = new URI(urlString);
+
+                    uriString = uri.toString();
+
+                    System.out.printf("Image uriString: %s%n", uriString);
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+
+                    continue;
+                } //end try catch
+
+                image = new Image(uriString);
+
+                imageView = new ImageView(image);
+
+                node = imageView;
+            } //end if
+
+            mediaName = String.valueOf(mediaCount);
+
+            mediaCount++;
+
+            scrollPane = new ScrollPane(node);
+
+            titledPane = new TitledPane(mediaName, scrollPane);
+
+            titledPanes.add(titledPane);
+        } //end for
+
+        array = new TitledPane[titledPanes.size()];
+
+        titledPanes.toArray(array);
+
+        accordion = new Accordion(array);
+
+        return accordion;
+    } //getMediaAccordion
+
     private VBox createPostBox(Status status, Scene scene) {
         String name;
         String screenName;
@@ -84,6 +202,8 @@ public final class TwitterExample extends Application {
         String combinedName;
         Label nameLabel;
         Text text;
+        MediaEntity[] mediaEntities;
+        Accordion accordion;
         String month;
         int day;
         int year;
@@ -93,6 +213,10 @@ public final class TwitterExample extends Application {
         String dateTimeString;
         Label dateTimeLabel;
         VBox vBox;
+
+        Objects.requireNonNull(status, "the specified status is null");
+
+        Objects.requireNonNull(scene, "the specified scene is null");
 
         name = status.getUser()
                      .getName();
@@ -120,6 +244,10 @@ public final class TwitterExample extends Application {
         text.wrappingWidthProperty()
             .bind(scene.widthProperty());
 
+        mediaEntities = status.getMediaEntities();
+
+        accordion = getMediaAccordion(mediaEntities);
+
         month = dateTime.getMonth()
                         .name();
 
@@ -142,7 +270,14 @@ public final class TwitterExample extends Application {
 
         dateTimeLabel.setFont(Font.font("Arial", FontWeight.BOLD, 12));
 
-        vBox = new VBox(nameLabel, text, dateTimeLabel);
+        if (accordion == null) {
+            vBox = new VBox(nameLabel, text, dateTimeLabel);
+        } else {
+            accordion.prefWidthProperty()
+                     .bind(scene.widthProperty());
+
+            vBox = new VBox(nameLabel, text, accordion, dateTimeLabel);
+        } //end if
 
         return vBox;
     } //createPostBox
