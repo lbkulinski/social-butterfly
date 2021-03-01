b95fa724309c1969e59c4ee69836baaa571a454d
Anudeep Yakkala <anudeepyakkala@anudeeps-mbp.lan>
2021-03-01 15:22:30 -0800

    Add functionality that allows a user to post, reply to, and like a tweet.

--- com/src/main/java/socialbutterfly/UserRequests.java
+++ com/src/main/java/socialbutterfly/UserRequests.java
@@ -3,6 +3,7 @@ package socialbutterfly;
 import java.util.List;
 
 import twitter4j.Status;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.TwitterException;
@@ -40,5 +41,28 @@ public class UserRequests {
             result += status.getUser().getName() + ": " + status.getText() + "\n\n";
         }
         return result;
+    }
+
+    Status postTweet(StatusUpdate statusUpdate) throws TwitterException {
+        /** Posts a new tweet to the user's account containing 
+         * the information in statusUpdate. If a replytostatusid 
+         * is included in statusUpdate, this tweet will be added 
+         * as a reply to the specified tweet. 
+         * 
+         * @return status object that is created with the text 
+         * paramater
+         */
+        Status status = twitter.updateStatus(statusUpdate);
+        return status;
+    }
+
+    Status favoriteTweet(long id) throws TwitterException {
+        /** Favorites the status specified by the id 
+         *  paramater. 
+         * 
+         * @return the favorited status
+         */
+        Status status = twitter.createFavorite(id);
+        return status;
     }
 }
