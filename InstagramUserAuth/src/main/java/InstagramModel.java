d7c708244b3c3322687dd9e2cab61ef75ceb4d10
Anudeep Yakkala <anudeepyakkala@anudeeps-mbp.lan>
2021-03-01 17:49:49 -0800

    Add functionality that allows users to make a post, comment on a post, and like a post.

--- InstagramUserAuth/src/main/java/InstagramModel.java
+++ InstagramUserAuth/src/main/java/InstagramModel.java
@@ -4,8 +4,15 @@ import com.github.instagram4j.instagram4j.exceptions.IGLoginException;
 import com.github.instagram4j.instagram4j.models.media.timeline.TimelineMedia;
 import com.github.instagram4j.instagram4j.models.user.Profile;
 import com.github.instagram4j.instagram4j.requests.feed.FeedTimelineRequest;
+import com.github.instagram4j.instagram4j.requests.media.MediaActionRequest;
+import com.github.instagram4j.instagram4j.requests.media.MediaCommentRequest;
+import com.github.instagram4j.instagram4j.requests.media.MediaActionRequest.MediaAction;
+import com.github.instagram4j.instagram4j.responses.IGResponse;
 import com.github.instagram4j.instagram4j.responses.feed.FeedTimelineResponse;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.rmi.activation.ActivationGroupDesc.CommandEnvironment;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -26,6 +33,10 @@ public class InstagramModel {
             System.out.println("\n\n\n");
         }
         */
+
+        String id = instagramModel.post("path to image file", "My first post!", client);
+        instagramModel.comment(id, "My first comment!", client);
+        instagramModel.like(id, client);
     }
 
     public IGClient login(String username, String password) {
@@ -90,5 +101,51 @@ public class InstagramModel {
             }
         }
         return posts;
+    }
+    private class IdObject {
+        String id;
+        public IdObject(String id) {
+            this.id = id;
+        }
+    }
+
+    public String post(String fileName, String caption, IGClient client) {
+        /** Creates a post on the user's account with the 
+         *  specified image file and the caption paramater. 
+         * 
+         * @return id of the new post
+         */
+
+        File file = new File(fileName);
+        IdObject idObj = new IdObject("");
+
+        client.actions().timeline()
+        .uploadPhoto(file, caption)
+        .thenAccept(res -> {
+            idObj.id = res.getMedia().getId();
+        }).join();
+
+        return idObj.id;
+    }
+
+    public boolean comment(String postId, String message, IGClient client) {
+        /** Adds a comment with the specified message
+         *  to the post corrosponding to postId.
+         * 
+         * @return if the comment was added successfully 
+         */
+        IGResponse response = new MediaCommentRequest(postId, message).execute(client).join();
+        return response.getStatus().equals("ok");
+    }
+
+    public boolean like(String postId, IGClient client) {
+        /** Adds a like to the post corrosponding
+         * to postId.
+         * 
+         * @return if the like was added successfully
+         */
+
+        IGResponse response = new MediaActionRequest(postId, MediaAction.LIKE).execute(client).join();
+        return response.getStatus().equals("ok");
     }
 }
